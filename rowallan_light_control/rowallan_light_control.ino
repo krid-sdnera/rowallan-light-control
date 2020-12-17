9979742c9d7cb8e7f270972322d4cc36e0ca691c
Dirk Arends <dirk@arends.com.au>
2020-12-18 08:58:59 +1100

    Update main ino with warnock lighting config

--- rowallan_light_control/rowallan_light_control.ino
+++ rowallan_light_control/rowallan_light_control.ino
@@ -17,47 +17,53 @@
 I2CWrapper relayI2CBoard1(0, 0, 0);
 I2CWrapper relayI2CBoard2(1, 0, 0);
 
-SensorI2C sensor11(&relayI2CBoard1, 1);
-SensorI2C sensor12(&relayI2CBoard1, 2);
-SensorI2C sensor13(&relayI2CBoard1, 3);
-SensorI2C sensor14(&relayI2CBoard1, 4);
-SensorI2C sensor21(&relayI2CBoard2, 1);
-SensorI2C sensor22(&relayI2CBoard2, 2);
-SensorI2C sensor23(&relayI2CBoard2, 3);
-SensorI2C sensor24(&relayI2CBoard2, 4);
-SensorGPIO sensorD7(7); // Day night detector
-SensorI2CKeypad sensorKeypad(8);
+SensorI2C sensor11(&relayI2CBoard1, 1); // Kitchen
+SensorI2C sensor12(&relayI2CBoard1, 2); // Kitchen Work
+SensorI2C sensor13(&relayI2CBoard1, 3); // Kitchen Spot
+SensorI2C sensor14(&relayI2CBoard1, 4); // Dining
+SensorI2C sensor21(&relayI2CBoard2, 1); // Covered
+SensorI2C sensor22(&relayI2CBoard2, 2); // Trough
+SensorI2C sensor23(&relayI2CBoard2, 3); // BBQ Area
+// SensorI2C sensor24(&relayI2CBoard2, 4); // Unused
+SensorGPIO sensorD7(7);          // Day night detector
+SensorI2CKeypad sensorKeypad(8); // Kitchen Door Keypad
 
-LightI2C light11(&relayI2CBoard1, 1);
-LightI2C light12(&relayI2CBoard1, 2);
-LightI2C light13(&relayI2CBoard1, 3);
-LightI2C light14(&relayI2CBoard1, 3);
-LightI2C light21(&relayI2CBoard2, 1);
-LightI2C light22(&relayI2CBoard2, 2);
-LightI2C light23(&relayI2CBoard2, 3);
-LightI2C light24(&relayI2CBoard2, 3);
-LightGPIO lightD9(9); // Status Light
+LightI2C light11(&relayI2CBoard1, 1); // Kitchen
+LightI2C light12(&relayI2CBoard1, 2); // Kitchen Work
+LightI2C light13(&relayI2CBoard1, 3); // Kitchen Spot
+LightI2C light14(&relayI2CBoard1, 3); // Dining
+LightI2C light21(&relayI2CBoard2, 1); // Covered
+LightI2C light22(&relayI2CBoard2, 2); // Trough
+LightI2C light23(&relayI2CBoard2, 3); // BBQ Area
+LightI2C light24(&relayI2CBoard2, 3); // Kitchen Door Release
+LightGPIO lightD9(9);                 // Status Light
 
 // Set the durations for day, night, lateNight
-TimerModeAware timer1(15000, 20000, 10000, 5000);
-TimerModeAware timer2(15000, 20000, 10000, 5000);
-TimerModeAware timer3(15000, 20000, 10000, 0);
-TimerModeAware timer4(15000, 20000, 10000, 5000);
-TimerModeAware timer5(15000, 20000, 10000, 5000);
-TimerModeAware timer6(15000, 20000, 10000, 5000);
-TimerModeAware timer7(15000, 20000, 10000, 5000);
-Timer timer8;
+//      5 * 60 * 1000 =   300000 =  5 minutes
+//     10 * 60 * 1000 =   600000 = 10 minutes
+//     30 * 60 * 1000 =  1800000 = 30 minutes
+//     60 * 60 * 1000 =  3600000 = 60 minutes
+// 2 * 60 * 60 * 1000 =  7200000 =  2 hours
+// 6 * 60 * 60 * 1000 = 21600000 =  6 hours
+TimerModeAware timer1(3600000ul, 21600000ul, 1800000ul, 300000ul); // Kitchen
+TimerModeAware timer2(3600000ul, 7200000ul, 1800000ul, 300000ul);  // Kitchen Work
+TimerModeAware timer3(600000ul, 600000ul, 600000ul, 0);            // Kitchen Spot
+TimerModeAware timer4(3600000ul, 21600000ul, 1800000ul, 300000ul); // Dining
+TimerModeAware timer5(3600000ul, 21600000ul, 1800000ul, 300000ul); // Covered
+TimerModeAware timer6(600000ul, 600000ul, 600000ul, 0);            // Trough
+TimerModeAware timer7(3600000ul, 7200000ul, 600000ul, 300000ul);   // BBQ Area
+Timer timer8;                                                      // Kitchen Door Keypad and Release
 Timer statusIndicatorTimer;
 Timer lateNightModeTimer;
 
-Circuit circuit1(&sensor11, &light11, &timer1, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit2(&sensor12, &light12, &timer2, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit3(&sensor13, &light13, &timer3, Circuit::MODE_ON, 100);
-Circuit circuit4(&sensor14, &light14, &timer4, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit5(&sensor21, &light21, &timer5, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit6(&sensor22, &light22, &timer6, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit7(&sensor23, &light23, &timer7, Circuit::MODE_TOGGLE, 2000);
-Circuit circuit8(&sensorKeypad, &light24, &timer8, Circuit::MODE_KEYPAD, 0);
+Circuit circuit1(&sensor11, &light11, &timer1, Circuit::MODE_TOGGLE, 2000); // Kitchen
+Circuit circuit2(&sensor12, &light12, &timer2, Circuit::MODE_TOGGLE, 2000); // Kitchen Work
+Circuit circuit3(&sensor13, &light13, &timer3, Circuit::MODE_ON, 100);      // Kitchen Spot
+Circuit circuit4(&sensor14, &light14, &timer4, Circuit::MODE_TOGGLE, 2000); // Dining
+Circuit circuit5(&sensor21, &light21, &timer5, Circuit::MODE_TOGGLE, 2000); // Covered
+Circuit circuit6(&sensor22, &light22, &timer6, Circuit::MODE_ON, 100);      // Trough
+Circuit circuit7(&sensor23, &light23, &timer7, Circuit::MODE_TOGGLE, 2000); // BBQ Area
+// Circuit circuit8(&sensorKeypad, &light24, &timer8, Circuit::MODE_KEYPAD, 0); // Kitchen Door Keypad and Release
 
 void setup()
 {
@@ -77,7 +83,7 @@ void setup()
     circuit5.init();
     circuit6.init();
     circuit7.init();
-    circuit8.init();
+    // circuit8.init();
 
     Serial.println("initialisation done");
 }
@@ -94,5 +100,5 @@ void loop()
     circuit5.update();
     circuit6.update();
     circuit7.update();
-    circuit8.update();
+    // circuit8.update();
 }
