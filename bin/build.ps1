0145d4bf51073d89b38012e3859b54bf8250d9b3
Dirk Arends <dirk@arends.com.au>
2020-12-31 01:22:44 +1100

    Update build script to handle vendor files

--- bin/build.ps1
+++ bin/build.ps1
@@ -18,7 +18,7 @@ if (!(Test-Path  "src\inos\rowallan_light_control_${target}.ino")) {
 
 echo "> Removing previous build"
 Get-ChildItem build\* -Recurse | Remove-Item
-New-Item -Path "build\" -Name "rowallan_light_control" -ItemType "directory" -Force
+$null = New-Item -Path "build\" -Name "rowallan_light_control" -ItemType "directory" -Force
 
 echo "> Copying ino for ${target}"
 Copy-Item -Path "src\inos\rowallan_light_control_${target}.ino" -Destination "build\rowallan_light_control\rowallan_light_control.ino"
@@ -26,6 +26,10 @@ Copy-Item -Path "src\inos\rowallan_light_control_${target}.ino" -Destination "bu
 echo "> Copying libs"
 Copy-Item -Path "src\libs\*.cpp" -Destination "build\rowallan_light_control\"
 Copy-Item -Path "src\libs\*.h" -Destination "build\rowallan_light_control\"
+
+echo "> Copying vendor libs"
+Copy-Item -Path "src\vendor\*.cpp" -Destination "build\rowallan_light_control\"
+Copy-Item -Path "src\vendor\*.h" -Destination "build\rowallan_light_control\"
 
 echo "> Dry run: Compiling project"
 arduino-cli compile --fqbn arduino:avr:uno build\rowallan_light_control
@@ -38,6 +42,6 @@ if ($confirm -ne "y" -And $confirm -ne "Y") {
 }
 
 echo "> Uploading project"
-arduino-cli upload -p /dev/${port} --fqbn arduino:avr:uno build\rowallan_light_control
+arduino-cli compile --upload --port ${port} --fqbn arduino:avr:uno build\rowallan_light_control
 
 echo "< Done"
